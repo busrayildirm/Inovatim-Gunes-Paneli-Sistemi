a5b98b57c92deedb0d220591d8639b6f3a287bf9
busrayildirm <busrayildirim11895@gmail.com>
2026-03-28 00:25:51 +0300

    proje kodları eklendi

---
+++ güneşpaneli/güneşpaneli/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using güneşpaneli.Models;
+
+namespace güneşpaneli.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Ana Sayfa
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Hakkımızda Sayfası
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // İletişim Sayfası
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        // Privacy sayfası (isteğe bağlı)
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+     
+       }
+        // Hesaplama Sayfası
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+    }
+}
